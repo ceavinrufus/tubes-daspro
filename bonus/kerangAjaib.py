95a308a2b859febbe5fe095ced635836076df41c
ceavinrufus <18221162@std.stei.itb.ac.id>
2022-04-11 05:29:44 +0700

    kerangAjaib first commit

---
+++ bonus/kerangAjaib.py
@@ -0,0 +1,24 @@
+import time
+from arraytools import panjang
+
+def lcg(x, m):
+    # Fungsi yang mereturn bilangan acak dengan menggunakan metode linear congruential generator
+
+    # KAMUS LOKAL
+    # a, c, x : integer
+
+    # ALGORITMA
+    a = 5
+    c = time.gmtime().tm_sec        # Nilai c merupakan nilai detik
+
+    x = (a*x + c) % m               # Rumus linear congruentiaal generator
+
+    return x
+
+if input() == "kerangajaib":
+    input("Apa pertanyaanmu? ")
+
+    replies = ["hah", "gatau", "iyah", "hmm", 'g']
+
+    print()
+    print(replies[lcg(0, panjang(replies))])
