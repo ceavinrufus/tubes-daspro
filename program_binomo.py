e3328859ecfdb25596cad87a24c8d7ec20ffaa18
ceavinrufus <18221162@std.stei.itb.ac.id>
2022-04-12 11:04:25 +0700

    update program_binomo.py

--- program_binomo.py
+++ program_binomo.py
@@ -1,10 +1,4 @@
-from load import load
-from save import save
-from register import register
-from login import login
-from exit import exit
-from ubah_stok import ubah_stok
-from help import *
+from procedures import *
 
 # KAMUS
 # idx : integer
@@ -14,10 +8,10 @@ from help import *
 # ALGORITMA
 commands = ["register", "tambah_game", "ubah_game", "ubah_stok", "list_game_toko",
             "buy_game", "list_game", "search_my_game", "search_game_at_store", "topup",
-            "riwayat", "help", "save", "exit"]
+            "riwayat", "help", "save", "exit", "kerangajaib", "tictactoe"]
 user = [] ; game = [] ; riwayat = [] ; kepemilikan = []     # Inisiasi array
+load(user, game, riwayat, kepemilikan)                      # Load file ke dalam array
 
-load(user, game, riwayat, kepemilikan)      # Load file ke dalam array
 print('Selamat datang di antarmuka "Binomo"')
 
 while True:
@@ -27,49 +21,59 @@ while True:
         while idx >= 0:
             choose = input(">>> ")
             if choose in commands:
-                if user[idx][4] == "admin":           # Menu yang dapat dijalankan oleh admin
-                    if choose == "register":          # Hanya admin
+                if user[idx][4] == "admin":             # Menu yang dapat dijalankan oleh admin
+                    if choose == "register":            # Hanya admin
                         register(user)
-                    elif choose == "tambah_game":     # Hanya admin
+                    elif choose == "tambah_game":       # Hanya admin
                         pass
-                    elif choose == "ubah_game":       # Hanya admin
+                    elif choose == "ubah_game":         # Hanya admin
                         pass
-                    elif choose == "ubah_stok":       # Hanya admin
+                    elif choose == "ubah_stok":         # Hanya admin
                         ubah_stok(game)
-                    elif choose == "topup":           # Hanya admin
+                    elif choose == "topup":             # Hanya admin
                         pass
+                    elif choose == "help":
+                        help_admin()
                     elif choose == "list_game_toko":
                         pass
                     elif choose == "search_game_at_store":
                         pass
-                    elif choose == "help":
-                        help_admin()
                     elif choose == "save":
                         save(user, game, riwayat, kepemilikan)
                     elif choose == "exit":
                         exit(user, game, riwayat, kepemilikan)
                     else:
                         print("Maaf, anda harus menjadi user untuk melakukan hal tersebut.")
-                elif user[idx][4] == "user":          # Menu yang dapat dijalankan oleh user
-                    if choose == "buy_game":          # Hanya user
+                elif user[idx][4] == "user":            # Menu yang dapat dijalankan oleh user
+                    if choose == "buy_game":            # Hanya user
                         pass
-                    elif choose == "list_game":       # Hanya user
+                    elif choose == "list_game":         # Hanya user
                         pass
-                    elif choose == "search_my_game":  # Hanya user
+                    elif choose == "search_my_game":    # Hanya user
                         pass
-                    elif choose == "riwayat":         # Hanya user
+                    elif choose == "riwayat":           # Hanya user
                         pass
+                    elif choose == "kerangajaib":       # Hanya user
+                        replies = ["hah", "gatau", "iyah", "hmm", 'g']  # List jawaban yang bisa dikeluarkan
+                        kerangajaib(replies)
+                    elif choose == "tictactoe":         # Hanya user
+                        board = [['#', '#', '#'],
+                                 ['#', '#', '#'],
+                                 ['#', '#', '#']]
+                        tictactoe(board)
+                    elif choose == "help":
+                        help_user()
                     elif choose == "list_game_toko":
                         pass
                     elif choose == "search_game_at_store":
                         pass
-                    elif choose == "help":
-                        help_user()
                     elif choose == "save":
                         save(user, game, riwayat, kepemilikan)
                     elif choose == "exit":
                         exit(user, game, riwayat, kepemilikan)
                     else:
                         print("Maaf, anda tidak memiliki izin untuk menjalankan perintah berikut. Mintalah ke administrator untuk melakukan hal tersebut.")
+            else:
+                print('Tidak ada menu "{}"'.format(choose))
     else:
         print('Maaf, anda harus login terlebih dahulu untuk mengirim perintah selain "login"')
